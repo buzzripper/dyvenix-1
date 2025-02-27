a325e8c1fcb0983c384881ce756eb292ac7578cf
buzzripper <asmith88@outlook.com>
2025-02-26 22:57:43 -0500

    WIP

--- src/auth.core/Services/AuthSessionService.cs
+++ src/auth.core/Services/AuthSessionService.cs
@@ -2,6 +2,7 @@
 using Dyvenix.Auth.Core.SvcClients;
 using Microsoft.Extensions.Caching.Distributed;
 using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -29,15 +30,18 @@ public class AuthSessionService : IAuthSessionService
 	{
 		var tokenFromAuthCodeResponse = await _b2cSvcClient.GetTokenFromAuthCode(authCode, codeVerifier);
 
-		var idToken = JsonSerializer.Deserialize<IdToken>(tokenFromAuthCodeResponse.IdToken);
+		//var idToken = JsonSerializer.Deserialize<IdToken>(tokenFromAuthCodeResponse.IdToken);
+
+		var handler = new JwtSecurityTokenHandler();
+        var jwtDecoded = handler.ReadToken(tokenFromAuthCodeResponse.IdToken) as JwtSecurityToken;
+
+		var identityId = jwtDecoded.Subject;
+
 		var refreshToken = tokenFromAuthCodeResponse.RefreshToken;
 
 		var userExtId = GetIdToken(tokenFromAuthCodeResponse.IdToken);
 		if (string.IsNullOrEmpty(userExtId))
 			throw new ApplicationException("UserExtId is null or empty");
-
-
-
 
 		// Create a new session and store in cache
 		var sessionId = Guid.NewGuid().ToString();
